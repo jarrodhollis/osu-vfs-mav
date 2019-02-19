7ace997803671272cb8d0ab8a2186b3d89d4947a
Jarrod Hollis <hollisj@oregonstate.edu>
2019-02-18 20:13:23 -0800

    Accessing PCA object directly

--- pwm_generator.py
+++ pwm_generator.py
@@ -2,8 +2,8 @@ from adafruit_servokit import ServoKit
 
 def pwm_generator_main():
 	kit = ServoKit(channels=16)
-	kit.set_pwm_freq(60)
-	kit.set_pwm(0, 0, 491)
+	kit._pca.set_pwm_freq(60)
+	kit._pca.set_pwm(0, 0, 491)
 	while True:
 		True
 
