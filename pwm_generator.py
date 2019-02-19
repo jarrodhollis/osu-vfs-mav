9b2aa20f010759463fa06fa67252fa53cb155700
Jarrod Hollis <hollisj@oregonstate.edu>
2019-02-18 20:21:44 -0800

    Corrected code to use PCA9685 device

--- pwm_generator.py
+++ pwm_generator.py
@@ -1,9 +1,14 @@
-from adafruit_servokit import ServoKit
+import board
+import busio
+import adafruit_pca9685
+
 
 def pwm_generator_main():
-	kit = ServoKit(channels=16)
-	kit._pca.set_pwm_freq(60)
-	kit._pca.set_pwm(0, 0, 491)
+	i2c = busio.I2C(board.SCL, board.SCA)
+	hat = addafruit_pca9685.PCA9685(i2c)
+	hat.frequency = 60
+	pwm_channel = hat.channels[0]
+	pwm_channel.duty_cycle = 7864
 	while True:
 		True
 
