c09b30f1dcf7f34866844715ea91a35b4ce6bd2e
Jarrod Hollis <hollisj@oregonstate.edu>
2019-02-18 20:24:14 -0800

    Corrected typo (addafruit to adafruit)

--- pwm_generator.py
+++ pwm_generator.py
@@ -5,7 +5,7 @@ import adafruit_pca9685
 
 def pwm_generator_main():
 	i2c = busio.I2C(board.SCL, board.SDA)
-	hat = addafruit_pca9685.PCA9685(i2c)
+	hat = adafruit_pca9685.PCA9685(i2c)
 	hat.frequency = 60
 	pwm_channel = hat.channels[0]
 	pwm_channel.duty_cycle = 7864
