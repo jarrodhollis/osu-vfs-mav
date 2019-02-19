acc419063e1e91b4a6d7d1ec55cc64adb7756e48
Jarrod Hollis <hollisj@oregonstate.edu>
2019-02-19 00:15:11 -0800

    Adjusted test code to print both sample and output packets

--- control_driver.py
+++ control_driver.py
@@ -21,7 +21,7 @@ def translate_packet(control_packet):
 		- control_packet['yaw'] * 0.5, 0, 1, PULSE_WIDTH_MIN, PULSE_WIDTH_MAX)
 	translated_packet['servo_pitch'] = control_packet['pitch']
 	translated_packet['servo_roll'] = control_packet['roll']
-	translated_packet['servo_hook'] = 1 - 2 * control_packet['engage_hook']
+	translated_packet['servo_hook'] = 2 * control_packet['engage_hook'] - 1
 	return translated_packet
 
 def linear_map(value, from_min, from_max, to_min, to_max):
@@ -44,6 +44,11 @@ def control_driver_main():
 			output_packet = translate_packet(control_packet)
 			sample_available = False
 
+		print("---sample packet---")
+		for key in control_packet:
+			print(key + ": " + str(control_packet[key]))
+
+		print("---output packet---")
 		for key in output_packet:
 			print(key + ": " + str(output_packet[key]))
 
